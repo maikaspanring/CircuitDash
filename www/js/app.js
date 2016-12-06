d6781e683825b72f99f760a41b2af8ed53fb8a74
Lackner Lisa [HAGLEITNER] <lisa.lackner@HAGLEITNER.COM>
2016-12-06 11:56:30 +0100

    Menu handling and first assets
    Signed-off-by: Lackner Lisa [HAGLEITNER] <lisa.lackner@HAGLEITNER.COM>

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,7 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    ionic.Platform.fullScreen();
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -18,7 +19,7 @@ angular.module('starter', ['ionic'])
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
-      StatusBar.styleDefault();
+      StatusBar.hide();
     }
   });
 })
